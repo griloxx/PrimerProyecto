a1f36dbf9a7136e5781eb9d4932d60cfc9890cf7
jairoweb <jairodhweb89@outlook.es>
2023-06-21 20:51:38 +0200

    logica de Ahorcado terminada

--- principal.js
+++ principal.js
@@ -1,72 +1,70 @@
 "use strict";
-
+import {
+  aumentoNumeroIntentos,
+  devuelveSolucionParcial,
+  seleccionarPalabraAleatoria,
+  existeLetra,
+  generarGuion,
+} from "./funciones.js";
 //Creo un array con las palabras con las que se va a jugar.
 
-let words = [
-  "feo",
-  "hoyuelo",
-  "festejo",
-  "unicornio",
-  "figuritas",
-  "lagartija",
-  "tramar",
-  "atletismo",
-  "facultad",
-  "loro",
-  "puntual",
-  "humear",
-  "cuerpo",
-  "anunciar",
-  "horno",
-  "alianza",
-  "hiedra",
-  "dictador",
-  "elegante",
-  "estribo",
-  "respaldo",
-  "vestuario",
-  "dibujar",
-  "palmera",
-  "adelgazar",
-  "inmenso",
-  "plumas",
-  "congelador",
-  "parte",
-  "establo",
-  "fotocopia",
-  "cuchillo",
-  "radio",
-  "levantar",
-  "bidet",
-  "ayer",
-  "gradas",
-];
-
-//función para  Seleccionar la palabra aleatoria del array
-export function seleccionarPalabraAleatoria() {
-  const palabraAleatoria = Math.floor(Math.random() * (words.length + 1));
-  let palabra = words[palabraAleatoria];
-  return palabra;
-}
-export let palabra2 = seleccionarPalabraAleatoria();
-//función para crear los _ _ _ de la palabra seleccionada
-
-export function generarGuion(palabra2) {
-  return "_".repeat(palabra2.length);
-}
-export let guionesBajos = generarGuion(palabra2);
-
-//Selecciono parrafo con querySelector
+const maximoNumeroDeIntentos = 6;
+let finPartida;
+let numeroDeintentos;
+let solucionParcial;
+let solucion;
+// Selecciono parrafo con querySelector
 const palabraSeleccionada = document.querySelector(".palabraAleatoria");
 
-export function añadirGuiones(guiones) {
+function inicializar() {
   let botonStart = document.querySelector(".comenzar");
   botonStart.addEventListener("click", (event) => {
     event.preventDefault();
-    const guiones2 = document.querySelector(".guionesBajos");
-    palabraSeleccionada.textContent = `La palabra secreta es: `;
-    palabraSeleccionada.classList.add("parrafoNuevo");
-    guiones2.textContent = `${guiones}`;
-    guiones2.classList.add("parrafoNuevo");
+    finPartida = false;
+    solucion = seleccionarPalabraAleatoria();
+    numeroDeintentos = 0;
+    solucionParcial = generarGuion(solucion);
+    palabraSeleccionada.textContent = `La palabra secreta es: ( ${solucionParcial} )`;
+
+    console.log(solucion);
+  });
+
+  let botonComenzar = document.querySelector("#comprobar");
+  botonComenzar.addEventListener("click", (event) => {
+    event.preventDefault();
+    let valor = document.querySelector("#letra").value;
+    comprobar(valor);
+    document.querySelector("#letra").value = "";
+    // 1. Obtener la letra del elemento input
+    // 2. Llamar a la funcion comprobar
   });
 }
+
+function comprobar(letra) {
+  console.log(solucionParcial);
+  if (!finPartida) {
+    if (existeLetra(letra, solucion)) {
+      solucionParcial = devuelveSolucionParcial(
+        letra,
+        solucion,
+        solucionParcial
+      );
+      palabraSeleccionada.textContent = `La palabra secreta es: ( ${solucionParcial} )`;
+    } else {
+      numeroDeintentos = aumentoNumeroIntentos(numeroDeintentos);
+      palabraSeleccionada.textContent = `La letra no existe. Te quedan ${
+        maximoNumeroDeIntentos - numeroDeintentos
+      } intentos`;
+    }
+    console.log(solucionParcial === solucion);
+    if (numeroDeintentos === maximoNumeroDeIntentos) {
+      finPartida = true;
+      palabraSeleccionada.textContent = `Has perdido`;
+    } else if (solucionParcial === solucion) {
+      finPartida = true;
+      palabraSeleccionada.textContent = `Has ganado. La palabra secreta es ${solucion}`;
+    }
+  }
+}
+
+inicializar();
